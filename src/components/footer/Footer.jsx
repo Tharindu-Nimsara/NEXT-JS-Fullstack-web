a2d61fec2886cc6b37fdc6c41e5bf9be946de5bb
Tharindu <tharinduonline1080@gmail.com>
2026-01-30 14:33:28 +0530

    Navbar and Footer feature implemented

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>2026 brand. All rights reserverd</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
